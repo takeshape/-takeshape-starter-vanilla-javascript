939040c355677eae6433dbf7f2935247dc06bede
S3Prototype <liuqahs15@gmail.com>
2021-08-12 14:55:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# TakeShape Starter Vanilla JS
+
+The following is a guide to launching a Vanilla JS TakeShape project that injects Ecommerce data into HTML markup.
+
+
+## Instructions
+
+1. Create a TakeShape project with the pattern `Shape Shop`. [Checkout our docs on creating a project to learn more](https://app.takeshape.io/docs/get-started/create-project).
+
+2. Follow our documentation to [create a read-only API Key](https://app.takeshape.io/docs/api/api-keys).
+
+3. Find your API Endpoint by navigating to your project in the TakeShape web client and clicking on the Home tab. Scroll down, and you'll see your API Endpoint in the `Useful Snippets` section on the bottom right. Refer to the image below:
+
+![Useful Snippets](./images/useful-snippets.png)
+
+4. In the terminal on your local machine, clone this repo with `git clone https://github.com/takeshape/takeshape-starter-auth0.git.`
+
+5. Open the project directory and edit `takeshape-example.js` and replace the text that says `YOUR-ENDPOINT-HERE` with your API Endpoint. Replace the text that says `YOUR-API-KEY-HERE` with your API Key.
+
+6. Open the `index.html` file in the root of the project directory in your favorite browser.
+
+
+## Learn More
+
+Check out [our docs on this project](https://app.takeshape.io/docs/get-started/client/javascript) to learn how this example works.
--- takeshape-example.js
+++ takeshape-example.js
@@ -1,13 +1,11 @@
 (async ()=> {
-    try{
-        const headers = new Headers()
-        
+    try{        
         const result = await fetch(
             'https://api.takeshape.io/project/6cdbcc52-5697-475b-872e-fcb6fd0e00d0/v3/graphql',
             {
                 method: 'POST',
                 headers: {
-                    Authentication:'Bearer 6091d7bc2d8946e290486e1526483eda', 
+                    'Authorization':'Bearer 6091d7bc2d8946e290486e1526483eda', 
                     'Content-Type':'application/json'
                 },
                 body: JSON.stringify({
@@ -23,23 +21,21 @@
                 })
             }
         )
-
-        console.log(result)
-    
+        
         const resultJSON = await result.json()
-
-        const container = document.getElementById('resultsContainer')
+        
+        const containerList = document.getElementById('resultsContainer')
 
         const {items} = resultJSON.data.getProductList
 
-        for(const product in items){
+        for(const product of items){
+            console.log(product)
             const listItem = document.createElement('li')
-            const innerText = document.createTextNode(product.name + " : " + product.price)
+            const innerText = document.createTextNode(product.name + " : $" + product.price)
             listItem.appendChild(innerText)
             listItem.setAttribute('id', product._id)
-            container.appendChild(listItem)
+            containerList.appendChild(listItem)
         }
-
     } catch(error) {
         console.log("TakeShape fetch Error!", error)
     }
